a739c42d4c2b8864b0fab5bf7619ecbf0bc64b88
nealkumar <nkumar20@jcu.edu>
2019-09-28 21:08:05 -0400

    Update ServiceConstants.java

--- backend/services/hoa-services/src/main/java/hoa/api/services/ServiceConstants.java
+++ backend/services/hoa-services/src/main/java/hoa/api/services/ServiceConstants.java
@@ -13,7 +13,7 @@ public abstract class ServiceConstants {
 //<<<<<<< HEAD
 //	public final static String CONNECTION_STRING = "";
 //=======
-	public final static String CONNECTION_STRING = "jdbc:sqlserver://nealkdbserver.database.windows.net:1433;database=UtilityDB;user=neal@nealkdbserver;password=Steel2727;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
+	public final static String CONNECTION_STRING = COMPILATION FAILURE INTENTIONAL! OMMITED.
 //>>>>>>> 836b2f329058aa4fdb47a02c9b68db85ee0ffdb2
 //>>>>>>> 78cae505cfb4965061d0e0f67dd8ce49201dbbbf
 	public final static String SERVICES = "/services";
