f2c00931f06b169e868123d8040322f8b2e7f701
nealkumar <nkumar20@jcu.edu>
2019-11-30 00:33:30 -0500

    Update ServiceConstants.java

--- backend/services/hoa-services/src/main/java/hoa/api/services/ServiceConstants.java
+++ backend/services/hoa-services/src/main/java/hoa/api/services/ServiceConstants.java
@@ -5,7 +5,7 @@ public abstract class ServiceConstants {
 	//Main Endpoing
 	public final static String API_PREFIX = "hoa.api.ngrok.io";
 	//Connection String
-	public final static String CONNECTION_STRING = "jdbc:sqlserver://nealkdbserver.database.windows.net:1433;database=UtilityDB;user=neal@nealkdbserver;password=Steel2727;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
+	public final static String CONNECTION_STRING = DOOOOOM
 	//Service home endpoint
 	public final static String SERVICES = "/services";
 	
